d91041f8c0f6364df58dda97fb2bc05038df94cc
Amr Nabih <amrnabih112@gmail.com>
2026-05-16 10:21:49 +0300

    feat: finshing billing /scedulling features

---
+++ src/MechanicShop.Application/Features/Billing/Queries/GetInvoiceById/GetInvoiceByIdQuery.cs
@@ -0,0 +1,17 @@
+using MechanicShop.Application.common.Interfaces;
+using MechanicShop.Application.Features.Billing.Dtos;
+using MechanicShop.Domain.Common.Results;
+
+namespace MechanicShop.Application.Features.Billing.Queries.GetInvoiceById;
+
+public sealed record GetInvoiceByIdQuery(Guid InvoiceId) : ICachedQuery<Result<InvoiceDto>>
+{
+    public string CacheKey => $"invoice_{InvoiceId}";
+
+    public TimeSpan Expiration => TimeSpan.FromMinutes(10);
+
+    public string[] Tags => ["invoice"];
+
+    TimeSpan? ICachedQuery.Expiration => Expiration;
+
+}
